d03c5f22cb4659a3359b8391bf16ea67373b4c90
Abel Prieto Martín <aprieto@IBERAVAL.local>
2026-04-01 17:13:50 +0200

    añadido modelo Clasificaciones | insertado view_form de Clasificaciones

--- modules/students/models/models.py
+++ modules/students/models/models.py
@@ -1,36 +1,33 @@
 from odoo import models, fields
-
 class Students(models.Model):
-        _name = "students.info"
-        _description = "Estudiantes (Udemy)"
+    _name = "students.info"
+    _description = "Estudiantes (Udemy)"
+    _rec_name = "estudiante_id"
         
-        # nombre = fields.Char(string="Nombre del estudiante")
-        # apellido = fields.Char(string="Apellidos")
-        # edad = fields.Integer(string="Edad")
-        # email = fields.Char(string="Email")
-        
-        estudiante_id = fields.Many2one("res.partner", string="Estudiante", required=True)
-
-        # curso = fields.Char(string="Curso")
-        curso_id = fields.Many2one("cursos.students", string="Curso")
-        
-        fecha_nacimiento = fields.Date(string="Fecha de Nacimiento")
-        calificacion = fields.Float(string="Calificación")
-        estado_calificacion = fields.Selection(
-             [
-                 {"suspenso","Suspenso"},
-                 {"aprobado","Aprobado"},
-                 {"notable","Notable"},
-                 {"sobresaliente","Sobresaliente"}
-             ]
-        )
-        esta_aprobado = fields.Boolean(string="¿Está aprobado?", default=False)
-        comentarios = fields.Html(string="Comentarios")
-        
+    estudiante_id = fields.Many2one("res.partner", string="Estudiante", required=True)
+    curso_id = fields.Many2one("cursos.students", string="Curso")
+    esta_aprobado = fields.Boolean(string="¿Está aprobado?", default=False)
+    comentarios = fields.Html(string="Comentarios")
+    fecha_nacimiento = fields.Date(string="Fecha de Nacimiento")
+    # calificacion = fields.Float(string="Calificación")
+    calificaciones_ids = fields.One2many("students.calificaciones", "estudiante_id", string="Calificaciones")
+    country_id = fields.Many2one("res.country", string="País", related='estudiante_id.country_id')
+    tag_id = fields.Many2many("res.partner.category", string="Etiquetas")
+    address = fields.Char(string="URL Google")
+    estado_calificacion = fields.Selection(
+        [
+            ("suspenso","Suspenso"),
+            ("aprobado","Aprobado")
+        ]
+    )
+    
 class Cursos(models.Model):
     _name = "cursos.students"
     _description = "Cursos (Udemy)"
+    _rec_name = "nombre_curso"
     
+    asignatura_ids = fields.Many2many("students.asignaturas", string="Asignaturas",)
+    profesor_id = fields.Many2one("students.profesores", string="Tutor", required=True)
     nombre_curso = fields.Selection([
         ('primero_a', '1A'),
         ('primero_b', '1B'),
@@ -42,4 +39,26 @@ class Cursos(models.Model):
         ('cuarto_b', '4B')
     ], string="Curso")
     
+class Asignaturas(models.Model):
+    _name = "students.asignaturas"
+    _description = "Asignaturas (Udemy)"
+    _rec_name = "nombre_asignatura"
+    
+    nombre_asignatura = fields.Char(string="Asignatura", required=True)
+    
+class Profesores(models.Model):
+    _name = "students.profesores"
+    _description = "Profesores (Udemy)"
+    _rec_name = "nombre_profesor"
+    
+    nombre_profesor = fields.Many2one("res.partner", string="Profesor", required=True)
+    
+class Calificaciones(models.Model):
+    _name = "students.calificaciones"
+    _description = "Calificaciones (Udemy)"
+    _rec_name = "calificacion"
+    
+    estudiante_id = fields.Many2one("students.info", string="Estudiante")
+    asignatura_id = fields.Many2one("students.asignaturas", string="Asignatura")
+    calificacion = fields.Float(string="Calificación")
     
